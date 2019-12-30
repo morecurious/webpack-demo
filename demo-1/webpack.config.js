ab1b23aca8bcaef890e5393b9381c4d729d772ca
morecurious <1033620647@qq.com>
2019-12-30 17:12:44 +0800

    feat:test

---
+++ demo-1/webpack.config.js
@@ -0,0 +1,50 @@
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+    entry: {
+        app: './main.js'
+    },
+    output: {
+        path: path.resolve(__dirname, './dist'),
+        filename: '[name].js',
+    },
+    devServer: {
+        open: true,
+        // port: 9000
+    },
+    externals: {
+        jquery: 'jQuery',
+        jquery: '$'
+    },
+    module: {
+        rules: [
+            {
+                test: require.resolve('underscore'),
+                use: 'imports-loader?_=underscore'
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader']
+            }
+        ]
+    },
+    plugins: [
+        new webpack.ProvidePlugin({
+            __: 'lodash',
+            '$':'jquery'
+        }),
+        new HtmlWebpackPlugin({
+            title: 'webpack-demo-1',
+            template: './template/index.html'
+        }),
+        new CopyWebpackPlugin([
+            {
+              from: path.resolve(__dirname, 'dist/'),
+              to: path.resolve(__dirname, 'static/')
+            }
+        ])
+    ]
+}
